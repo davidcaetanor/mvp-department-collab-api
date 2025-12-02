3c69e6350aeb0cf7104fb07ab8874cb0e8ae6b10
daka1 <davidcaetanoribeiro@gmail.com>
2025-12-02 17:33:35 -0300

    first commit

---
+++ src/main/java/br/com/mvp_backend_departments_collabs/mvp_backend_departments_collabs/MvpBackendDepartmentsCollabsApplication.java
@@ -0,0 +1,13 @@
+package br.com.mvp_backend_departments_collabs.mvp_backend_departments_collabs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvpBackendDepartmentsCollabsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MvpBackendDepartmentsCollabsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/mvp_backend_departments_collabs/mvp_backend_departments_collabs/MvpBackendDepartmentsCollabsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.mvp_backend_departments_collabs.mvp_backend_departments_collabs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MvpBackendDepartmentsCollabsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

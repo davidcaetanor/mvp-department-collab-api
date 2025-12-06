02671199a70ddaf2a92d53de91985fe5a5601189
daka1 <davidcaetanoribeiro@gmail.com>
2025-12-06 16:11:55 -0300

    feat: controller e service iniciais da class collaborator

--- src/main/java/br/com/mvp_backend_departments_collabs/mvp_backend_departments_collabs/collaborator/controller/CollaboratorController.java
+++ src/main/java/br/com/mvp_backend_departments_collabs/mvp_backend_departments_collabs/collaborator/controller/CollaboratorController.java
@@ -1,4 +1,46 @@
 package br.com.mvp_backend_departments_collabs.mvp_backend_departments_collabs.collaborator.controller;
 
+import br.com.mvp_backend_departments_collabs.mvp_backend_departments_collabs.collaborator.dto.CollaboratorDTO;
+import br.com.mvp_backend_departments_collabs.mvp_backend_departments_collabs.collaborator.service.CollaboratorService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("collaborators")
 public class CollaboratorController {
+
+    private CollaboratorService collaboratorService;
+
+    public CollaboratorController(CollaboratorService collaboratorService) {
+        this.collaboratorService = collaboratorService;
+    }
+
+    @GetMapping
+    public List<CollaboratorDTO> collaboratorFindALL(){
+        return collaboratorService.getALLCollaborator();
+    }
+
+    @PostMapping
+    public CollaboratorDTO collaboratorSave(@RequestBody CollaboratorDTO dto){
+        return collaboratorService.saveCollaborator(dto);
+    }
+
+    @PutMapping("{id}")
+    public Optional<CollaboratorDTO> collaboratorUpdate(@PathVariable UUID id, @RequestBody CollaboratorDTO dto){
+        return collaboratorService.updateCollaborator(id, dto);
+    }
+
+    @GetMapping("{id}")
+    public Optional<CollaboratorDTO> collaboratorFindById(@PathVariable UUID id){
+        return collaboratorService.getCollaboratorByID(id);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteCollaborator(@PathVariable UUID id){
+        collaboratorService.deleteCollaborator(id);
+    }
+
 }
---
+++ src/main/java/br/com/mvp_backend_departments_collabs/mvp_backend_departments_collabs/collaborator/service/CollaboratorService.java
@@ -0,0 +1,62 @@
+package br.com.mvp_backend_departments_collabs.mvp_backend_departments_collabs.collaborator.service;
+
+import br.com.mvp_backend_departments_collabs.mvp_backend_departments_collabs.collaborator.dto.CollaboratorDTO;
+import br.com.mvp_backend_departments_collabs.mvp_backend_departments_collabs.collaborator.mapper.CollaboratorMapper;
+import br.com.mvp_backend_departments_collabs.mvp_backend_departments_collabs.collaborator.model.CollaboratorModel;
+import br.com.mvp_backend_departments_collabs.mvp_backend_departments_collabs.collaborator.repository.CollaboratorRepository;
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.ui.Model;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@Service
+public class CollaboratorService {
+    private CollaboratorRepository collaboratorRepository;
+    private CollaboratorMapper collaboratorMapper;
+
+    public CollaboratorService(CollaboratorRepository collaboratorRepository, CollaboratorMapper collaboratorMapper) {
+        this.collaboratorRepository = collaboratorRepository;
+        this.collaboratorMapper = collaboratorMapper;
+    }
+
+    public CollaboratorDTO saveCollaborator(CollaboratorDTO dto){
+        CollaboratorModel collabModel;
+        collabModel = collaboratorMapper.mapToModel(dto);
+        collaboratorRepository.save(collabModel);
+        return collaboratorMapper.mapToDTO(collabModel);
+    }
+
+    public List<CollaboratorDTO> getALLCollaborator(){
+        List<CollaboratorModel> models = collaboratorRepository.findAll();
+        return models.stream()
+                .map(collaboratorMapper::mapToDTO)
+                .toList();
+    }
+
+    public Optional<CollaboratorDTO> getCollaboratorByID(UUID id){
+        Optional<CollaboratorModel> modelOptional = collaboratorRepository.findById(id);
+        return modelOptional.map(collaboratorMapper::mapToDTO);
+    }
+
+    public Optional<CollaboratorDTO> updateCollaborator(UUID id, CollaboratorDTO dto) {
+        Optional<CollaboratorModel> collaboratorExists = collaboratorRepository.findById(id);
+        Optional<CollaboratorDTO> collaboratorDTO = Optional.empty();
+        if (collaboratorExists.isPresent()) {
+            CollaboratorModel upCollaborator = collaboratorMapper.mapToModel(dto);
+            upCollaborator.setIdCollaborator(id);
+            collaboratorRepository.save(upCollaborator);
+            collaboratorDTO = Optional.ofNullable(collaboratorMapper.mapToDTO(upCollaborator));
+        }
+        return collaboratorDTO;
+    }
+
+    public void deleteCollaborator(UUID id){
+        if (collaboratorRepository.existsById(id)){
+            collaboratorRepository.deleteById(id);
+        }
+    }
+}
